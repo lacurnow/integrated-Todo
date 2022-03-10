c2267aed7cdb502518dc9442b06fc8b09f98b499
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-03-10 14:13:16 +0000

    Added rspec files

---
+++ spec/integration_spec.rb
@@ -0,0 +1,10 @@
+require 'Todo'
+require 'TodoList'
+
+RSpec.describe "integration" do
+  context "when we add a todo task" do
+    it "adds that task to the todo list" do
+      todo_list = TodoList.new
+    end
+  end
+end
